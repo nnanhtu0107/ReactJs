3b67c26eee9f7a2f262836399da80f8470990020
nnanhtu0107 <nnanhtu017@gmail.com>
2021-11-08 18:33:52 +0700

    update modal

---
+++ src/containers/System/ModalUser.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import { Modal, ModalBody, ModalFooter, ModalHeader, Button } from "reactstrap";
+class ModalUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+  toggle = () => {
+    this.props.toggleFromParent();
+  };
+  render() {
+    console.log("check child props", this.props);
+    console.log("check child open modal", this.props.isOpen);
+    return (
+      <Modal
+        isOpen={this.props.isOpen}
+        toggle={() => {
+          this.toggle();
+        }}
+        className={"modal-user-container"}
+        size="lg"
+        centered
+      >
+        <ModalHeader
+          toggle={() => {
+            this.toggle();
+          }}
+        >
+          Create a new user
+        </ModalHeader>
+        <ModalBody>
+          <div className="modal-user-body">
+            <div className="input-container">
+              <label>Email</label>
+              <input type="text" />
+            </div>
+            <div className="input-container">
+              <label>Password</label>
+              <input type="password" />
+            </div>
+            <div className="input-container">
+              <label>First Name</label>
+              <input type="text" />
+            </div>
+            <div className="input-container">
+              <label>Last Name</label>
+              <input type="text" />
+            </div>
+            <div className="input-container max-w-input">
+              <label>Address</label>
+              <input type="text" />
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="primary"
+            onClick={() => {
+              this.toggle();
+            }}
+            className="px-3"
+          >
+            Save changes
+          </Button>
+          {""}
+          <Button
+            color="secondary"
+            onClick={() => {
+              this.toggle();
+            }}
+            className="px-3"
+          >
+            Close
+          </Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ModalUser);
